4ddee723cb3a0bf16e36da75fdae6445137267d4
Obiora Igboanusi <cwertlinks@gmail.com>
2021-05-17 01:50:40 +0100

    fix error

--- scripts/app.js
+++ scripts/app.js
@@ -4,19 +4,23 @@ const navLinks = document.getElementById("nav-links");
 const nav = document.getElementById("nav");
 
 
-document.addEventListener("click", (e) => {
-  let expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
-  if (expanded && e.target !== navLinks) {
-    navLinks.classList.remove("nav-active");
-    navToggle.classList.remove("toggle");
-    navToggle.setAttribute("aria-expanded", !expanded);
-  } else if (e.target === navToggle) {
-    toggleNav();
-  }
-});
+navToggle.addEventListener('click', toggleNav)
+
+
+// document.addEventListener("click", (e) => {
+//   let expanded = navToggle.getAttribute("aria-expanded") === 'true' || false;
+//   if (expanded) {
+//     if (e.target !== navLinks) {
+//       navLinks.classList.remove("nav-active");
+//       navToggle.classList.remove("toggle");
+//       navToggle.setAttribute("aria-expanded", !expanded);
+//     } 
+//   }
+// })
+
 
 function toggleNav() {
-  let expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
+  let expanded = navToggle.getAttribute("aria-expanded") === 'true' || false;
   navLinks.classList.toggle("nav-active");
   navToggle.classList.toggle("toggle");
   navToggle.setAttribute("aria-expanded", !expanded);
