81270f3e000d8516cf9b9896d781cc97a836ab59
Obiora Igboanusi <cwertlinks@gmail.com>
2021-05-17 01:24:31 +0100

    adjusted styles

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,23 @@
-'use strict'
-const navToggle = document.getElementById('burger');
-const navLinks = document.getElementById('nav-links');
-let expanded = navToggle.getAttribute('aria-expanded') === 'true' || false
-navToggle.addEventListener('click', () => {
-  
-  navLinks.classList.toggle('nav-active');
-  navToggle.classList.toggle('toggle');
-  navToggle.setAttribute('aria-expanded', !expanded);
-  console.log(expanded)
-})
+"use strict";
+const navToggle = document.getElementById("burger");
+const navLinks = document.getElementById("nav-links");
+const nav = document.getElementById("nav");
+
+
+document.addEventListener("click", (e) => {
+  let expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
+  if (expanded && e.target !== navLinks) {
+    navLinks.classList.remove("nav-active");
+    navToggle.classList.remove("toggle");
+    navToggle.setAttribute("aria-expanded", !expanded);
+  } else if (e.target === navToggle) {
+    toggleNav();
+  }
+});
+
+function toggleNav() {
+  let expanded = navToggle.getAttribute("aria-expanded") === "true" || false;
+  navLinks.classList.toggle("nav-active");
+  navToggle.classList.toggle("toggle");
+  navToggle.setAttribute("aria-expanded", !expanded);
+}
